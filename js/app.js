79c7f78f926c148d76125193bda0b2383047d2dc
Demid Ganenko <ganenko94@gmail.com>
2016-04-11 02:04:46 +0300

    Initial commit, uploading project

---
+++ js/app.js
@@ -0,0 +1,43 @@
+/*
+ * Author: Demyd Ganenko
+ * GitHub: https://github.com/Eisenheim94
+ * Email: ganenko94@gmail.com
+ */
+
+var app = angular.module("task", ["ngStorage"]);
+
+app.controller("TaskCtrl", function($scope, $localStorage, $filter) {
+
+	
+	$scope.posts = $localStorage.posts;
+	
+	$scope.currentPost = $scope.posts.length > 0 ? $scope.posts[0] : false;
+	
+	$scope.items = [];
+
+	$scope.addPost = function(newPost) {
+		
+		newPost.comments = [];
+		
+		newPost.parent = angular.isUndefined(newPost.parent) ? false : newPost.parent;
+		
+		$scope.posts.push(angular.copy(newPost));
+		
+		$scope.loadPost(newPost);
+		
+		$localStorage.posts = $scope.posts;
+		
+	}
+
+	$scope.addComment = function(addComm) {
+		
+		$scope.currentPost.comments.push(angular.copy(addComm));
+		
+	}
+
+	$scope.loadPost = function(post) {
+		
+		$scope.currentPost = post;
+		
+	}
+});
